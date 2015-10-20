dbae71b62cafed1282a99474c99d04035dab9e5c
netcodeMario <mp208983@gmail.com>
2015-10-20 04:45:33 -0400

    Great Changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var userA = prompt("Rate my Webpage?")
+var userA = prompt("Password?");
+
+if (userA = "coding is great") {
+  confirm("Go right in!");
+};
+
+else {
+  confirm("You can still go in. I don't know enough to block a website.");
+};
