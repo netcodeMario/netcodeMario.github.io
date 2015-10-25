1198fde1458fcfffa09760ab4e076d179de7427f
netcodeMario <mp208983@gmail.com>
2015-10-25 17:58:24 -0400

    Back to Original
    Default

--- script.js
+++
@@ -1,9 +0,0 @@
-var userA = prompt("Password?");
-
-if (userA = "coding is great") {
-  confirm("Go right in!");
-};
-
-else {
-  confirm("You can still go in. I don't know enough to block a website.");
-};
